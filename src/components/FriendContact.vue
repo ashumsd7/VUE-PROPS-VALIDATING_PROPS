a6feab498899bbe549a16bf925e161116a246929
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2021-05-25 20:40:03 +0530

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  //use this or : props : simple
+
+  // props:[
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress'
+  // ],
+
+  //like this : props as validator
+  props:{
+    name:{
+      type:String,
+      required:true,
+      default:"Arjun Sachin Tendulkar"
+    },
+    phoneNumber:String,
+    emailAddress:String
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      // friend: {
+      //   id: "manuel",
+      //   name: "Manuel Lorenz",
+      //   phone: "0123 45678 90",
+      //   email: "manuel@localhost.com",
+      // },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
